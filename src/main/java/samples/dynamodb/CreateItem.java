73a605d1bbc82d42b6d2bdf812eb9c6f0f1baf81
Eduardo Bohrer <nbluisrs@gmail.com>
2013-09-14 18:24:38 -0300

    creating user with mapper

--- src/main/java/samples/dynamodb/CreateItem.java
+++ src/main/java/samples/dynamodb/CreateItem.java
@@ -1,9 +1,13 @@
 package samples.dynamodb;
 
+import samples.dynamodb.model.User;
+
 public class CreateItem extends DynamoDBSample {
 
 	public void run() {
-
+		User user = new User(1L, "Eduardo");
+		mapper.save(user);
+		System.out.println("User created!");
 	}
 
 	public static void main(String[] args) {
--- src/main/java/samples/dynamodb/model/User.java
+++ src/main/java/samples/dynamodb/model/User.java
@@ -1,8 +1,38 @@
 package samples.dynamodb.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "user")
 public class User {
+
+	private Long id;
+	private String name;
+
+	public User() {}
+
+	public User(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@DynamoDBHashKey(attributeName = "id")
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@DynamoDBRangeKey(attributeName = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
